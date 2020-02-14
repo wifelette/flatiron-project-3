20b65d091f3dee9d11b8de4a6535efb2bb9a3574
wifelette <LeahSilber@gmail.com>
2020-02-13 17:26:32 -0800

    Finished scope methods for current/past views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,15 @@
 class Event < ApplicationRecord
   has_many :packages
+  scope :not_yet_happened, -> { where("start_date > ?", Time.now) } 
   
-  validates :name, presence: true
+  validates :name, uniqueness: true, presence: true
+
+  # This is effectively the same thing as the Scope bit above, I just wanted to practice both ways.
+  def self.already_happened
+    Event.where("start_date < ?", Time.now)
+  end
+
+  # def self.not_yet_happened
+  #   Event.where("start_date > ?", Time.now)
+  # end
 end
