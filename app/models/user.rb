fadfac35c2030f997cfd85af5f9fc37c8e023c46
wifelette <LeahSilber@gmail.com>
2020-02-13 17:25:37 -0800

    Nixed email uniqueness in favor of Devise defaults

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 end
