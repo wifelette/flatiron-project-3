826db6e0e14ab4a8ecf176172e9993b91d62f185
Leah Silber <LeahSilber@gmail.com>
2020-02-17 17:12:08 -0800

    Added some docs

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,8 @@ class Event < ApplicationRecord
 
   # This is effectively the same thing as the Scope bit above, I just wanted to practice both ways.
   def self.already_happened
-    Event.where("start_date < ?", Time.now)
+    # At first I had `Event.where`, but I don't need it because self inside of a class is itself, so I removed it.
+    where("start_date < ?", Time.now)
   end
 
   # def self.not_yet_happened
